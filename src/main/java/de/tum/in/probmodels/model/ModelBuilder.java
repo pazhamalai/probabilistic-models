f309e68c287d664104439d40dfa5f12c263e8b28
Tobias Meggendorfer <tobias@meggendorfer.de>
2019-11-20 15:27:59 +0100

    More

--- src/main/java/de/tum/in/probmodels/model/ModelBuilder.java
+++ src/main/java/de/tum/in/probmodels/model/ModelBuilder.java
@@ -1,9 +1,25 @@
 package de.tum.in.probmodels.model;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import de.tum.in.naturals.set.BoundedNatBitSet;
+import de.tum.in.naturals.set.NatBitSet;
+import de.tum.in.naturals.set.NatBitSets;
+import de.tum.in.naturals.unionfind.IntArrayUnionFind;
+import de.tum.in.naturals.unionfind.IntUnionFind;
 import de.tum.in.probmodels.generator.Choice;
 import de.tum.in.probmodels.generator.Generator;
+import it.unimi.dsi.fastutil.ints.IntSet;
 import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Queue;
+import java.util.function.IntFunction;
+import java.util.function.IntUnaryOperator;
+import java.util.function.Supplier;
 import parser.State;
 
 public final class ModelBuilder {
@@ -40,5 +56,125 @@ public final class ModelBuilder {
       }
     }
     return stateIndices;
+  }
+
+  public static <T extends Model> QuotientModel<T> buildQuotient(T model,
+      Supplier<T> quotientModelConstructor, List<NatBitSet> equivalence) {
+    T quotientModel = quotientModelConstructor.get();
+    checkArgument(quotientModel.getNumStates() == 0);
+    int numStates = model.getNumStates();
+    IntUnionFind collapseUF = new IntArrayUnionFind(numStates);
+
+    NatBitSet[] stateClass = new NatBitSet[numStates];
+    equivalence.forEach(states -> {
+      int representative = states.firstInt();
+      states.forEach((int state) -> {
+        collapseUF.union(representative, state);
+        stateClass[state] = states;
+      });
+    });
+
+    int[] stateToQuotientArray = new int[numStates];
+    Arrays.fill(stateToQuotientArray, -1);
+    for (int state = 0; state < numStates; state++) {
+      int representative = collapseUF.find(state);
+      int quotientState = stateToQuotientArray[representative];
+      if (quotientState > 0) {
+        stateToQuotientArray[state] = quotientState;
+      } else {
+        int newQuotientState = quotientModel.addState();
+        stateToQuotientArray[representative] = newQuotientState;
+        stateToQuotientArray[state] = newQuotientState;
+      }
+    }
+    assert quotientModel.getNumStates() == collapseUF.componentCount();
+
+    Multimap<Integer, Object> selfLoops = HashMultimap.create();
+    IntUnaryOperator stateToQuotientState = i -> stateToQuotientArray[i];
+    for (int state = 0; state < numStates; state++) {
+      int quotientState = stateToQuotientArray[state];
+      for (Action action : model.getActions(state)) {
+        Distribution quotientDistribution = action.distribution().map(stateToQuotientState);
+        if (quotientDistribution.size() == 1 && quotientDistribution.contains(quotientState)) {
+          if (selfLoops.put(quotientState, action.label())) {
+            quotientModel.addChoice(quotientState, Action.of(quotientDistribution, action.label()));
+          }
+        } else {
+          quotientModel.addChoice(quotientState, Action.of(quotientDistribution, action.label()));
+        }
+      }
+    }
+    model.getInitialStates().forEach((int initialState) ->
+        quotientModel.addInitialState(stateToQuotientArray[initialState]));
+
+    NatBitSet[] quotientToClassArray = new NatBitSet[quotientModel.getNumStates()];
+    for (int i = 0; i < numStates; i++) { // NOPMD
+      int quotientState = stateToQuotientArray[i];
+      assert quotientToClassArray[quotientState] == null
+          || quotientToClassArray[quotientState] == stateClass[i];
+      quotientToClassArray[quotientState] = stateClass[i];
+      assert quotientToClassArray[quotientState].contains(i);
+    }
+
+    IntFunction<NatBitSet> quotientToClass = i -> quotientToClassArray[i];
+    return QuotientModelTuple.create(quotientModel, quotientToClass, stateToQuotientState);
+  }
+
+  public static <T extends Model> RestrictedModel<T> buildRestrictedModel(T model,
+      Supplier<T> restrictedModelConstructor, IntSet states) {
+    T newModel = restrictedModelConstructor.get();
+    checkArgument(newModel.getNumStates() == 0);
+    int[] originalToRestrictedStates = new int[model.getNumStates()];
+    Arrays.fill(originalToRestrictedStates, -1);
+    states.forEach((int allowedState) ->
+        originalToRestrictedStates[allowedState] = newModel.addState());
+
+    int restrictedStates = newModel.getNumStates();
+    assert restrictedStates == states.size();
+    int[] restrictedToOriginalStates = new int[restrictedStates];
+    NatBitSet[] restrictedActions = new NatBitSet[restrictedStates];
+
+    for (int originalState = 0; originalState < model.getNumStates(); originalState++) {
+      if (originalToRestrictedStates[originalState] == -1) {
+        continue;
+      }
+      int restrictedState = originalToRestrictedStates[originalState];
+      restrictedToOriginalStates[restrictedState] = originalState;
+
+      List<Action> actions = model.getActions(originalState);
+      BoundedNatBitSet removedActions = NatBitSets.boundedSet(actions.size(), 0);
+      ListIterator<Action> iterator = actions.listIterator();
+      while (iterator.hasNext()) {
+        int index = iterator.nextIndex();
+        Action action = iterator.next();
+
+        Distribution distribution = new Distribution();
+        action.distribution().forEach((target, probability) -> {
+          int restrictedDestination = originalToRestrictedStates[target];
+          if (restrictedDestination >= 0) {
+            distribution.add(restrictedDestination, probability);
+          }
+        });
+        if (distribution.isEmpty()) {
+          removedActions.set(index);
+        } else {
+          model.addChoice(restrictedState, Action.of(distribution.scale(), action.label()));
+        }
+      }
+      restrictedActions[restrictedState] = NatBitSets.compact(removedActions.complement());
+      /* if (addedActions == 0) {
+        newModel.addDeadlockState(restrictedState);
+      } */
+    }
+    model.getInitialStates().forEach((int initialState) -> {
+      int restrictedInitialState = originalToRestrictedStates[initialState];
+      if (restrictedInitialState != -1) {
+        newModel.addInitialState(restrictedInitialState);
+      }
+    });
+
+    IntUnaryOperator stateMapping = i -> restrictedToOriginalStates[i];
+    IntFunction<NatBitSet> stateActions = i -> restrictedActions[i];
+    return RestrictedModelTuple.create(newModel, stateMapping, stateActions);
   }
 }
